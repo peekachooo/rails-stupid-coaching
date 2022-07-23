e5dcd708f6c5a95796e838fd76064338e437ae5e
Jonathan Tay <jonathan.taysc@gmail.com>
2022-07-23 14:09:20 +0800

    Basic function done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = respond_to_question(params[:question])
+  end
+
+
+private
+
+  def respond_to_question(question)
+    if question == 'I am going to work'
+      "Great"
+    elsif question.end_with?('?')
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
